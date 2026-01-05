8622f6040b34896e9bfc6e43c66d97cd09efeaf6
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2026-01-05 01:11:56 -0300

    Suno semana 1 y 2 (2026)

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 41',
-    data: [16, 17, 22, 25], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 1',
+    data: [12, 13, 11, 22], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'],// Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 42',
-    data: [10, 12, 20, 18], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 2',
+    data: [15, 16, 10, 25], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
